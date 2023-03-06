312018b0c06cb805c86731d5840b2e145556c69a
emmaspeh <espeh@wustl.edu>
2023-03-06 13:27:05 -0600

    Update PlotSlicesTimeTrace.m

--- Functions/Visualizations/PlotSlicesTimeTrace.m
+++ Functions/Visualizations/PlotSlicesTimeTrace.m
@@ -1,4 +1,4 @@
-function PlotSlicesTimeTrace_230214(underlay, infoVol, params, overlay, info)
+function PlotSlicesTimeTrace_AS_230306(underlay, infoVol, params, overlay, info)
 
 % PLOTSLICESTIMETRACE Creates an interactive 4D plot.
 %
@@ -432,18 +432,10 @@ while ~any(button == [2, 27, 81, 113]) % 2 = middle mouse button, 27 = Esc, 81 =
             % New syntax for plotting red line 2/7/23
             TT_max = max(ydata);
             TT_min = min(ydata);
-            
-            % need to check if max and min are equal 
-            % because if PSTT initializes to a place where there is no data
-            % the max and min of the time trace will both be 0
-            % which will cause an error when we set the axis limits to be
-            % based off of TT_max and TT_min (line 462)
-            if TT_max == TT_min 
-                TT_max = TT_max +1;
-            end
-            
             plot(S(4) * ones(1,2), [TT_min - (0.1*abs(TT_min)),TT_max + 0.1*abs(TT_max)], '-r')
             hold off
+            
+            ylim([TT_min - (0.1*abs(TT_min)),TT_max + 0.1*abs(TT_max)])
         
         else
             plot([xlim, NaN, xlim], [ylim, NaN, flip(ylim)], '-r');
@@ -459,7 +451,7 @@ while ~any(button == [2, 27, 81, 113]) % 2 = middle mouse button, 27 = Esc, 81 =
             xlabel('Time (samples)', 'Color', LineColor, 'FontSize', 10)
         end
         ylabel('[Hb]', 'Color', LineColor, 'FontSize', 10)
-        ylim([TT_min - (0.1*abs(TT_min)),TT_max + 0.1*abs(TT_max)])
+        
         
         
         %% Add a colorbar to bottom of whole thing.
