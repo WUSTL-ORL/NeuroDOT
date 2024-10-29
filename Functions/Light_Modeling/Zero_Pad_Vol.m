27b870cdf22700e80546299b7fed9fd0b1ee0a1d
emmaspeh <espeh@wustl.edu>
2024-10-29 15:25:21 -0500

    Update Zero_Pad_Vol.m

--- Functions/Light_Modeling/Zero_Pad_Vol.m
+++ Functions/Light_Modeling/Zero_Pad_Vol.m
@@ -1,11 +1,20 @@
-function IM2=Zero_Pad_Vol(IM,Np)
+function [IM2, info2] = Zero_Pad_Vol(IM,info1,Np)
 %
 % This function takes an image volume and adds zeros at each edge.
 % For now, the volume is assumed to be 3D and the padding is uniform for
 % all 6 sides (padding = Np voxels).
 
+% Inputs
+% IM: image volume
+% info1: metadata for image volume
+% Np: number of pixels to pad image volume by on each side
+
+% Outputs
+% IM2: padded image volume
+% info2: metadata for padded image volume
+
 % Copyright (c) 2017 Washington University 
-% Created By: Adam T. Eggebrecht
+% Created By: Ari Segel
 % Eggebrecht et al., 2014, Nature Photonics; Zeff et al., 2007, PNAS.
 %
 % Washington University hereby grants to you a non-transferable, 
@@ -29,7 +38,22 @@ function IM2=Zero_Pad_Vol(IM,Np)
 % THE SOFTWARE, THE USE OF THE SOFTWARE, OR THIS AGREEMENT, WHETHER 
 % IN BREACH OF CONTRACT, TORT OR OTHERWISE, EVEN IF SUCH PARTY IS 
 % ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
-IM2=zeros(size(IM)+2*Np);
-IM2((Np+1):(end-Np),(Np+1):(end-Np),...
-    (Np+1):(end-Np))=IM;
+
+%% Copy info
+info2 = info1;
+
+%% Edit info structure with padding info
+% Dimensions first
+info2.nVx = info2.nVx + 2*Np;
+info2.nVy = info2.nVy + 2*Np;
+info2.nVz = info2.nVz + 2*Np;
+
+% Re-calculate center
+new_center = [info2.nVx, info2.nVy, info2.nVz] .* info2.mmppix;
+info2.center = new_center/2;
+
+%% Transform mask to add padding
+IM2 = affine3d_img(IM, info1, info2, eye(4), 'nearest');
+
+
 end 
