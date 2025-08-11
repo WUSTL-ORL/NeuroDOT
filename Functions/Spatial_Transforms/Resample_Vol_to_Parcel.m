6da6bb3e0903854c20c1ab5c2aeaf0b8f19f3bf3
emmaspeh <espeh@wustl.edu>
2025-08-11 17:54:50 -0500

    Update Resample_Vol_to_Parcel.m

--- Functions/Spatial_Transforms/Resample_Vol_to_Parcel.m
+++ Functions/Spatial_Transforms/Resample_Vol_to_Parcel.m
@@ -54,41 +54,41 @@ function Par_tt = Resample_Vol_to_Parcel(volume, Parcels, meshL, meshR, dim)
 % ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
 
 
-%% Parameters and Initialization      
-% Find the total number of unique parcels
-ParNum=length(unique(cat(1,Parcels.CtxL,Parcels.CtxR)))-1; 
-% Initialize time traces per parcel
-Par_tt = zeros(ParNum, size(volume,4));   
-
-
-
 %% Interpolate volumetric data onto the surface mesh 
 mapL = vol2surf_mesh(meshL, volume, dim); 
 mapR = vol2surf_mesh(meshR, volume, dim); 
+ParNum=max(cat(1,Parcels.CtxL,Parcels.CtxR)); 
+% Initialize time traces per parcel
+Par_tt = zeros(length(unique(cat(1,Parcels.CtxL,Parcels.CtxR))), size(volume,4));   
 
 
 %% Average volumetric data within each parcel
+indexpar = 0;
 for Par=1:ParNum
     idxP = find(Parcels.CtxL==Par);       % find the surface nodes within each parcel
     idx1 = any(mapL.data,2);              % find the surface nodes within the field of view
     idxD = find(idx1);
     idx = intersect(idxP,idxD);           % intersect parcel data and parcel field of view
     if (isempty(idx)==0)
+        indexpar = indexpar+1;
         mu=mean(mapL.data(idx,:));        % average the volumetric data within each parcel    
-        Par_tt(Par,:) = mu;
+        Par_tt(indexpar,:) = mu;
     end
     idxP = find(Parcels.CtxR==Par);
     idx1 = any(mapR.data,2);              % find the surface nodes within the field of view
     idxD = find(idx1);
     idx = intersect(idxP,idxD); 
     if (isempty(idx)==0)
+        indexpar = indexpar+1;
         mu=mean(mapR.data(idx,:));        % average the volumetric data within each parcel  
-        Par_tt(Par,:) = mu;
+        Par_tt(indexpar,:) = mu;
     end
 end
-
+deleteRows = all(Par_tt==0,2);
+Par_tt(deleteRows,:)=[];
 
 end
+
 
 %%
 
